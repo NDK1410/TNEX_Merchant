6c800c176093b198054fc3c99452ad2df79fb00b
NDK1410 <khanh.nd1410@gmail.com>
2020-12-15 02:08:12 -0800

    LoadingScreen

---
+++ lib/src/screens/LoadingScreen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:tnexmerchant/src/helpers/MyColors.dart';
+import 'package:tnexmerchant/src/screens/IntroScreen.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // return Container(
+    //   color: MyColors.loadingScreenBackground,
+    //   child: Center(
+    //     child: Image.asset(
+    //       "assets/Loading.png",
+    //       fit: BoxFit.cover,
+    //       width: 350,
+    //     ),
+    //   ),
+    // );
+    return Scaffold(body: buildBody(context));
+  }
+
+  Widget buildBody(BuildContext context) {
+    return FutureBuilder(
+      future: _access(),
+      builder: (context, snapshot) {
+        return Container(
+          color: MyColors.loadingScreenBackground,
+          child: Center(
+            child: Image.asset(
+              "assets/Loading.png",
+              fit: BoxFit.cover,
+              width: 350,
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Future<String> _access() async {
+    await Future.delayed(Duration(seconds: 4)).then((value) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (BuildContext context) {
+            return IntroScreen();
+          },
+        ),
+      );
+    });
+
+    return "Welcome to TNEX";
+  }
+}
