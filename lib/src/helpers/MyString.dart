68ae431c71ba1d9e7d304006f5141820d16b2832
NDK1410 <khanh.nd1410@gmail.com>
2020-12-15 00:27:15 -0800

    split-project-and-import-helpers

---
+++ lib/src/helpers/MyString.dart
@@ -0,0 +1,6 @@
+class MyString {
+  static var step1 = "Let's double your revenue in 3-6 months";
+  static var step2 = "Just a few steps and it's done";
+  static var step3 = "You are the only self";
+  static var step4 = "You will be proud to be part of MSB";
+}
