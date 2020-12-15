4d9cdde1c7e09f98bf6def2f04c5ebc783532205
NDK1410 <khanh.nd1410@gmail.com>
2020-12-15 03:16:51 -0800

    AddFirstProductScreen

--- lib/src/MyApp.dart
+++ lib/src/MyApp.dart
@@ -5,7 +5,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'TNEX Merchant App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
