7db6fa306508dbd518f5cc3e75250383b97181e9
NDK1410 <khanh.nd1410@gmail.com>
2020-12-22 10:29:44 +0700

    RegisterScreen

--- lib/src/MyApp.dart
+++ lib/src/MyApp.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:tnexmerchant/src/screens/LoadingScreen.dart';
-import 'package:tnexmerchant/src/screens/CampainSelectTemplate.dart';
+import 'package:tnexmerchant/src/screens/RegisterScreen.dart';
+import 'package:tnexmerchant/src/screens/IntroScreen.dart';
+import 'package:tnexmerchant/src/helpers/SlideRightRouter.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'TNEX Merchant App',
-      home: CampaignSelectTemplate(),
+      onGenerateRoute: (RouteSettings settings) {
+        switch (settings.name) {
+          case '/':
+            return SlideRightRoute(widget: IntroScreen());
+            break;
+          case '/register':
+            return SlideRightRoute(widget: RegisterScreen());
+            break;
+        }
+      },
+      home: LoadingScreen(),
     );
   }
 }
--- lib/src/helpers/MyString.dart
+++ lib/src/helpers/MyString.dart
@@ -3,4 +3,15 @@ class MyString {
   static var step2 = "Just a few steps and it's done";
   static var step3 = "You are the only self";
   static var step4 = "You will be proud to be part of MSB";
+  static var username_provide = "Please provide username";
+  static var all_errors =
+      "Please correct all the highlighted errors and try again";
+  static var password_provide = "Please provide a password";
+  static var phone_format = "Phone number is not in correct format";
+  static var stronger_password = "Please provide stronger password";
+  static var min_char = "more than 8 characters";
+  static var lower = "Lowercase";
+  static var upper_case = "Uppercase";
+  static var number_or_special = "Number or special characters";
+  static var username_require = "Only aphabet and number characters";
 }
