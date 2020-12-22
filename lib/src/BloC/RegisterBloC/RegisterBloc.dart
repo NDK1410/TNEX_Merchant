06b733db1beea53507f42669cd61e252af90240a
NDK1410 <khanh.nd1410@gmail.com>
2020-12-22 16:11:59 +0700

    Register-BloC

--- lib/src/BloC/RegisterBloC/RegisterBloc.dart
+++ lib/src/BloC/RegisterBloC/RegisterBloc.dart
@@ -1,6 +1,6 @@
-import 'dart:async';
-import 'package:bloc/bloc.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:tnexmerchant/src/validations/validation.dart';
+// import 'dart:async';
+// import 'package:bloc/bloc.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:tnexmerchant/src/validations/validation.dart';
 
-class RegisterBloc {}
+// class RegisterBloc {}
--- lib/src/screens/RegisterScreen.dart
+++ lib/src/screens/RegisterScreen.dart
@@ -19,7 +19,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
             ),
             Center(
               child: Container(
-                height: 400,
+                height: 500,
                 width: 320,
                 decoration: BoxDecoration(
                   color: Colors.white,
@@ -30,35 +30,105 @@ class _RegisterScreenState extends State<RegisterScreen> {
                   children: <Widget>[
                     Padding(
                       padding: EdgeInsets.only(top: 20, left: 20),
-                      child: Text("Username"),
+                      child: Text("Username", style: TextStyle(fontSize: 15)),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 5, left: 20, right: 20),
+                      child: TextField(
+                        decoration: new InputDecoration(
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Colors.blueAccent, width: 1.0),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.grey[400], width: 1.0),
+                          ),
+                        ),
+                      ),
                     ),
                     Padding(
                       padding: EdgeInsets.only(top: 20, left: 20),
-                      child: TextFormField(),
+                      child: Text("Phone", style: TextStyle(fontSize: 15)),
                     ),
                     Padding(
-                      padding: EdgeInsets.only(top: 10, left: 20),
-                      child: Text("Phone"),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 20, left: 20),
-                      child: TextFormField(),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 10, left: 20),
-                      child: Text("Password"),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 20, left: 20),
-                      child: TextFormField(),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 10, left: 20),
-                      child: Text("Confirm Password"),
+                      padding: EdgeInsets.only(top: 5, left: 20, right: 20),
+                      child: TextField(
+                        decoration: new InputDecoration(
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Colors.blueAccent, width: 1.0),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.grey[400], width: 1.0),
+                          ),
+                        ),
+                      ),
                     ),
                     Padding(
                       padding: EdgeInsets.only(top: 20, left: 20),
-                      child: TextFormField(),
+                      child: Text("Password", style: TextStyle(fontSize: 15)),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 5, left: 20, right: 20),
+                      child: TextField(
+                        decoration: new InputDecoration(
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Colors.blueAccent, width: 1.0),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.grey[400], width: 1.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20, left: 20),
+                      child: Text("Confirm Password",
+                          style: TextStyle(fontSize: 15)),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 5, left: 20, right: 20),
+                      child: TextField(
+                        decoration: new InputDecoration(
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Colors.blueAccent, width: 1.0),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.grey[400], width: 1.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20, left: 10),
+                      child: SizedBox(
+                        height: 50,
+                        width: 300,
+                        child: RaisedButton(
+                          color: MyColors.secondary,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(4),
+                            ),
+                          ),
+                          onPressed: () {
+                            // Navigator.pushNamed(context, '/register');
+                          },
+                          child: Text(
+                            "Create an account",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                      ),
                     ),
                   ],
                 ),
