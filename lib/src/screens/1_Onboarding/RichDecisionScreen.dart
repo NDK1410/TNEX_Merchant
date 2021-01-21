37e758ae8414108b385be9b26956bebed33f1ef7
NDK1410 <khanh.nd1410@gmail.com>
2021-01-21 18:04:19 +0700

    new-decision-screen

--- lib/src/screens/1_Onboarding/RichDecisionScreen.dart
+++ lib/src/screens/1_Onboarding/RichDecisionScreen.dart
@@ -1,91 +1,105 @@
 import 'package:flutter/material.dart';
 import 'package:tnexmerchant/src/helpers/MyColors.dart';
+import 'package:tnexmerchant/src/helpers/CustomClipPath.dart';
 
 class RichDecisionScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        /// Warning: Make Column Center at my Emulator ///
-        padding: EdgeInsets.symmetric(horizontal: 30),
-
-        /// Warning: Make Column Center at my Emulator ///
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(top: 50, bottom: 20),
-              child: Text(
-                "Want to be super rich ?",
-                style: TextStyle(
-                    color: MyColors.brand_dark,
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold),
-              ),
-            ),
-            Container(
-              height: 300,
-              width: 300,
-              child: Image.asset(
-                'assets/3-campaign/merchant-3-rich.png',
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 20),
-              child: Text(
-                "We have many tools to make you rich.",
-                style: TextStyle(
-                  color: MyColors.brand_dark,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 20),
-              child: Text(
-                "But first, please take ID Card photo.",
-                style: TextStyle(
-                  color: MyColors.brand_dark,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 70),
-              child: SizedBox(
-                height: 50,
-                width: 340,
-                child: RaisedButton(
-                  color: MyColors.brand_dark,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(4),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            color: Colors.grey[100],
+          ),
+          ClipPath(
+            clipper: CustomClipPath(),
+            child: Container(
+              padding: EdgeInsets.only(top: 50),
+              width: MediaQuery.of(context).size.width,
+              color: Colors.white,
+              height: 500,
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(top: 50),
+                    child: Text(
+                      "Let's become\na Super Merchant",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: MyColors.brand_dark,
+                        fontSize: 24,
+                        fontWeight: FontWeight.w700,
+                      ),
                     ),
                   ),
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/idcardtype');
-                  },
-                  child: Text(
-                    "Yes, I want to be a billionaire",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
+                  Container(
+                    height: 240,
+                    width: 180,
+                    child: Image.asset(
+                      'assets/1-register/merchant-3-rich.png',
                     ),
                   ),
-                ),
+                  Container(
+                    child: Text(
+                      "To become a Super Merchant with more\noffers, TNEX need you to provide your ID Card\nand face recognition",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: MyColors.textPrimary,
+                        fontSize: 14,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 30, top: 15),
-              child: Text(
-                "I will consider it later",
-                style: TextStyle(
-                  color: MyColors.brand_dark,
-                  fontSize: 20,
-                ),
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 450),
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(top: 70),
+                    child: SizedBox(
+                      height: 50,
+                      width: 340,
+                      child: RaisedButton(
+                        color: MyColors.primary,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(4),
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/idcardtype');
+                        },
+                        child: Text(
+                          "Capture ID Card",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 30, top: 15),
+                    child: Text(
+                      "I will provide later",
+                      style: TextStyle(
+                        color: MyColors.primary,
+                        fontSize: 14,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
