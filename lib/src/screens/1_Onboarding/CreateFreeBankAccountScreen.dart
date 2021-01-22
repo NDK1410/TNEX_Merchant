b1dca4b1e89bc50015da1d3d28b4c9fdafb89b36
NDK1410 <khanh.nd1410@gmail.com>
2021-01-22 13:34:52 +0700

    new-CreateFreeBankAccountScreen

--- lib/src/screens/1_Onboarding/CreateFreeBankAccountScreen.dart
+++ lib/src/screens/1_Onboarding/CreateFreeBankAccountScreen.dart
@@ -1,99 +1,126 @@
 import 'package:flutter/material.dart';
 import 'package:tnexmerchant/src/helpers/MyColors.dart';
+import 'package:tnexmerchant/src/helpers/CustomClipPath.dart';
 
 class CreateFreeBankAccountScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(top: 80, bottom: 20),
-              child: Text(
-                "Create free bank account",
-                style: TextStyle(
-                    color: Color.fromRGBO(0, 54, 104, 1),
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold),
-              ),
-            ),
-            Container(
-              height: 250,
-              child: Image.asset(
-                'assets/1-register/CreateFreeBankAccount.png',
-                fit: BoxFit.fill,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Icon(
-                    Icons.check,
-                    color: MyColors.success,
-                    size: 18,
-                  ),
-                  SizedBox(width: 10),
-                  Text(
-                    "Transactions with zero fees",
-                    style: TextStyle(
-                      color: Colors.black87,
-                      fontSize: 19,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Icon(
-                    Icons.check,
-                    color: MyColors.success,
-                    size: 18,
-                  ),
-                  SizedBox(width: 10),
-                  Text(
-                    "Online payment from TNEX consumers",
-                    style: TextStyle(
-                      color: Colors.black87,
-                      fontSize: 19,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Icon(
-                    Icons.check,
-                    color: MyColors.success,
-                    size: 18,
-                  ),
-                  SizedBox(width: 10),
-                  Text(
-                    "Get higher merchant rank",
-                    style: TextStyle(
-                      color: Colors.black87,
-                      fontSize: 19,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            color: Colors.grey[100],
+          ),
+          ClipPath(
+            clipper: CustomClipPath(),
+            child: Container(
               padding: EdgeInsets.only(top: 50),
+              width: MediaQuery.of(context).size.width,
+              color: Colors.white,
+              height: 500,
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    child: Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.only(top: 60, bottom: 20),
+                          child: Text(
+                            "Create free bank account",
+                            style: TextStyle(
+                              color: Color.fromRGBO(0, 54, 104, 1),
+                              fontSize: 24,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          height: 180,
+                          width: 230,
+                          child: Image.asset(
+                            'assets/1-register/CreateFreeBankAccount.png',
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 20),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(
+                                Icons.check,
+                                color: MyColors.success,
+                                size: 14,
+                              ),
+                              SizedBox(width: 5),
+                              Text(
+                                "Transactions with zero fees",
+                                style: TextStyle(
+                                  color: Colors.black87,
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(
+                                Icons.check,
+                                color: MyColors.success,
+                                size: 14,
+                              ),
+                              SizedBox(width: 5),
+                              Text(
+                                "Online payment from TNEX consumers",
+                                style: TextStyle(
+                                  color: Colors.black87,
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(
+                                Icons.check,
+                                color: MyColors.success,
+                                size: 14,
+                              ),
+                              SizedBox(width: 5),
+                              Text(
+                                "Get higher merchant rank",
+                                style: TextStyle(
+                                  color: Colors.black87,
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 430),
+            child: Center(
               child: SizedBox(
-                height: 50,
-                width: 340,
+                height: 40,
+                width: 280,
                 child: RaisedButton(
-                  color: MyColors.brand,
+                  color: MyColors.primary,
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.all(
                       Radius.circular(4),
@@ -106,24 +133,14 @@ class CreateFreeBankAccountScreen extends StatelessWidget {
                     "Create my bank account",
                     style: TextStyle(
                       color: Colors.white,
-                      fontSize: 19,
+                      fontSize: 14,
                     ),
                   ),
                 ),
               ),
             ),
-            Padding(
-              padding: EdgeInsets.only(top: 20),
-              child: Text(
-                "Request TNEX Merchant for account opening",
-                style: TextStyle(
-                  color: MyColors.brand,
-                  fontSize: 19,
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
